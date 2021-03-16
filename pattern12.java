dbef4d646d3892b0d075ecbd96eb7f5f0e390700
vaibhav <sonikumarvaibhava@gmail.com>
2021-03-16 13:56:46 +0530

    pyramid pattern  added

---
+++ pattern12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class pattern12
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of rows");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=n-1;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0;k<=i;k++)
+			{
+				System.out.print("*");
+			}
+			for(int l=1;l<=i;l++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
